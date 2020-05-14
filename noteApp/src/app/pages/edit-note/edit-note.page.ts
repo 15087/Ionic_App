6017f9cc1c4c7bb0c79a46306a76875781a940d3
Hana-Jabbour <36258812+Hana-Jabbour@users.noreply.github.com>
2020-05-14 14:13:37 +0200

    Edit Note
    Pb with module import fix

--- noteApp/src/app/pages/edit-note/edit-note.page.ts
+++ noteApp/src/app/pages/edit-note/edit-note.page.ts
@@ -1,4 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { LoadingController } from "@ionic/angular";
+import { NoteService } from "../../services/note.service";
+import { CategoryService } from "../../services/category.service";
+import { ActivatedRoute, Router } from "@angular/router";
+import {
+  FormControl,
+  FormGroupDirective,
+  FormBuilder,
+  FormGroup,
+  NgForm,
+  Validators,
+  FormArray,
+} from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
+
 
 @Component({
   selector: 'app-edit-note',
@@ -7,9 +21,97 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditNotePage implements OnInit {
 
-  constructor() { }
+  note = {
+    title: "Title",
+    content: "Content",
+    date: "Date",
+    category: "Category",
+  };
+  noteForm: FormGroup;
+  categories: any;
+  category: any;
 
-  ngOnInit() {
+  constructor(
+    public api: NoteService,
+    public apiCat: CategoryService,
+    public loadingController: LoadingController,
+    private route: ActivatedRoute,
+    public router: Router,
+    private formBuilder: FormBuilder
+  ) {
+    this.getCategories();
+    this.getNote(this.route.snapshot.paramMap.get("id"));
+    this.noteForm = this.formBuilder.group({
+      title: [null, Validators.required],
+      content: [null, Validators.required],
+      category: [null, Validators.required],
+    });
   }
 
+  ionViewWillEnter() {
+    this.getNote(this.route.snapshot.paramMap.get("id"));
+  }
+
+  async getCategories() {
+    const loading = await this.loadingController.create();
+    await loading.present();
+    await this.apiCat.getCategories()
+      .subscribe(res => {
+        console.log(res);
+        this.categories = res;
+        loading.dismiss();
+      }, err => {
+        console.log(err);
+        loading.dismiss();
+    });
+  }
+  async getNote(id) {
+    const loading = await this.loadingController.create({
+      message: "Loading",
+    });
+    await loading.present();
+    await this.api.getNote(id).subscribe(
+      (data) => {
+        this.note = {
+          title: data.title,
+          content: data.content,
+          date: data.date,
+          category: data.category,
+        };
+        loading.dismiss();
+      },
+      (err) => {
+        console.log(err);
+        loading.dismiss();
+      }
+    );
+  }
+  async editNote() {
+    const loading = await this.loadingController.create();
+    await loading.present();
+    this.category = this.noteForm.value.category.split("|");
+    this.noteForm.value.category = {
+      id: Number(this.category[0]),
+      name: this.category[1],
+    };
+    await this.api
+      .editNote(this.route.snapshot.paramMap.get("id"), this.noteForm.value)
+      .subscribe(
+        (data) => {
+          loading.dismiss();
+        },
+        (err) => {
+          console.log(err);
+          loading.dismiss();
+        }
+      );
+    this.redirect();
+  }
+
+  redirect() {
+    this.router.navigate(["/notes"]);
+  }
+  ngOnInit() {
+    this.getNote(this.route.snapshot.paramMap.get("id"));
+  }
 }
